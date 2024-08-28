288adc75379bbd3d435e819670a930ee651ce1fd
ailimecool <emilia.fredriksson2003@gmail.com>
2024-08-28 11:59:17 +0200

    live coding artwork example

---
+++ experiments/liveCodingNoise3.js
@@ -0,0 +1,26 @@
+function setup(){
+    createCanvas(600, 600);
+
+}
+
+const size = 10;
+const divider = 25;
+const numRows = 60;
+const numCols = 60;
+
+let counter = 0;
+
+function draw(){
+    background(255);
+    noStroke();
+    fill(0);
+
+   for(let y = 0; y < numRows; y++){
+    for(let x = 0; y < numCols; x++){
+        const value = noise(x / divider, y / divider) * size;
+        ellipse(size / 2 + x * size, size / 2 + y * size, value);
+    }
+   }
+
+counter += 0.1;
+}
